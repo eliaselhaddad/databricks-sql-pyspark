c86c77e936b35e2af348be9988fda38a0649e889
Elias El Haddad <86868035+eliaselhaddad@users.noreply.github.com>
2024-08-09 07:22:41 +0200

    Delete includes directory

--- includes/configuration.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-raw_folder_path = '/mnt/eliasf1deltalake/raw'
-processed_folder_path = '/mnt/eliasf1deltalake/processed'
-presentation_folder_path = '/mnt/eliasf1deltalake/presentation'
-
-# COMMAND ----------
-
-
