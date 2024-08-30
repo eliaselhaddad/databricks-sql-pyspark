944989738141301d4768c428c09a108418b35eec
eliaselhaddad <eliaselhaddad>
2024-08-30 06:44:19 +0000

    ingest pit stops, lap times and qualifying files

---
+++ ingestion/6.ingest_pit_stops_file.py
@@ -0,0 +1,69 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### Ingest pit_stops.json file
+
+# COMMAND ----------
+
+# MAGIC %run "../includes/configuration"
+
+# COMMAND ----------
+
+# MAGIC %run "../includes/common_functions"
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 1 - Read the JSON file using the spark dataframe reader API
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, FloatType
+
+pit_stops_schema = StructType(fields=[StructField("raceId", IntegerType(), False),
+                                      StructField("driverId", IntegerType(), True),
+                                      StructField("stop", StringType(), True),
+                                      StructField("lap", IntegerType(), True),
+                                      StructField("time", StringType(), True),
+                                      StructField("duration", StringType(), True),
+                                      StructField("milliseconds", IntegerType(), True)
+                                     ])
+
+# COMMAND ----------
+
+pit_stops_df = spark.read \
+    .option("multiLine", True) \
+    .option("inferSchema", True) \
+    .json(f"{raw_folder_path}/pit_stops.json")
+
+# Check the inferred schema
+pit_stops_df.printSchema()
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 2 - Rename columns and add new columns
+# MAGIC 1. Rename driverId and raceId
+# MAGIC 1. Add ingestion_date with current timestamp
+
+# COMMAND ----------
+
+pit_stops_with_ingestion_date_df = add_ingestion_date(pit_stops_df)
+
+# COMMAND ----------
+
+final_df = pit_stops_with_ingestion_date_df.withColumnRenamed("driverId", "driver_id") \
+                       .withColumnRenamed("raceId", "race_id")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 3 - Write to output to processed container in parquet format
+
+# COMMAND ----------
+
+final_df.write.mode("overwrite").parquet(f"{processed_folder_path}/pit_stops")
+
+# COMMAND ----------
+
+display(final_df)
